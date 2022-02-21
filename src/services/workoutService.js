27e7d66bb9ae722e478e07ec0a0e1f3455750922
bubment <medgyesiattila@elektromont.hu>
2022-02-21 12:55:45 +0100

    FIX test

--- src/services/workoutService.js
+++ src/services/workoutService.js
@@ -15,6 +15,7 @@ const startNewWorkout = async (psid) => {
     await User.addWorkout(psid,newWorkout._id)
     let warmupPostback = await getWarmupPostback(newWorkout,0)
     return warmupPostback;
+    //MEGNÉZNI EZT A MESSAGE OBJECTET
 }
 
 const comeBackLaterDefault = () => {
@@ -47,7 +48,7 @@ const createPostbackBody = (interaction,imageLink) => {
     let elementItem = {};
     if(interaction.title) elementItem.title = interaction.title;
     if(interaction.subtitle) elementItem.subtitle = interaction.subtitle;
-    if(imageLink) elementItem.image_url = imageLink;
+    // if(imageLink) elementItem.image_url = imageLink;
     if(interaction.options){
         elementItem.buttons = []
         interaction.options.forEach(option=>{
